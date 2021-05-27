f84ea5b8e24aa4add4c2596f0e00ef8fea68931b
Toàn Trần <neo290992@gmail.com>
2021-05-27 20:28:49 +0700

    tao

---
+++ my.js
@@ -0,0 +1,15 @@
+function check(){
+    let thang = document.getElementById('nhap').value;
+    
+    if (thang<12 & thang>1){
+    if(thang==2){
+        document.getElementById('ngay').innerHTML= 'Có 28 ngày vào tháng 2'
+    }else if (thang%2==0){
+        document.getElementById('ngay').innerHTML= 'có 30 ngày vào tháng'+ thang
+    }else{
+        document.getElementById('ngay').innerHTML= 'có 31 ngày vào tháng'+ thang
+    }
+}
+else {
+alert('lỗi')
+}}
